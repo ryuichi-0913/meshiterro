31ccf1e9a40c0039f389c761ad1c106ae4d81358
newda <newda@ryuichinoMacBook-Pro.local>
2020-08-20 18:31:29 +0900

    [finish]4-5

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'ca017057cd7f532fedfa588e0dae332359b0d782ac5d95c50a330d374712e4e237420babcc1251d5a68273b5b9cf386c040e4b749bb7403f0c9c44874506fa6a'
+
